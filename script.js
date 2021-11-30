e7104e5aed0ff5056442635e693c22c39f0029e2
Luiza Bozko Chagas <91279365+luizabchagas@users.noreply.github.com>
2021-11-30 09:09:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
-const btnMobile = document.querySelector("btn-mobile")
+const menuIcon = document.querySelector(".menu-icon")
 
-btnMobile.forEach(function (icons) {
-  icons.addEventListener("click", function () {
-    const openMenu = icons.classList.contains("active")
-    icons.forEach(function (icons) {
-      icons.classList.remove("active")
-    })
-    if (!openMenu) {
-      icons.classList.add("active")
-    } else {
-      icons.classList.remove("active")
-    }
-  })
-})
-
-/*const nav = document.getElementsByClassName("nav")
-
-function openMenu() {
-  const nav = document.getElementsByClassName("nav")
+menuIcon.addEventListener("click", function openMenu() {
+  const nav = document.querySelector(".nav")
   nav.classList.toggle("active")
-}*/
+})
