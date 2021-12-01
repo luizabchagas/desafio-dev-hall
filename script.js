467f19f1a3b5871a0b76acbcae5359fe2ca8b5f3
Luiza Bozko Chagas <91279365+luizabchagas@users.noreply.github.com>
2021-12-01 10:43:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+/* MENU HAMBURGUER */
+
 const menuIcon = document.querySelector(".menu-icon")
 
 menuIcon.addEventListener("click", function openMenu() {
   const nav = document.querySelector(".nav")
   nav.classList.toggle("active")
 })
+
+/* VALIDAÇÃO DO CAMPO NOME */
+
+function validateName() {
+  const name = document.getElementById("name").value
+  const error = document.getElementById("name-error")
+
+  error.innerHTML = ""
+
+  if (name.length < 2) {
+    error.innerHTML = "* O nome deve ter no mínimo 2 caracteres"
+    return false
+  }
+
+  return true
+}
